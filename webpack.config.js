10d2311774c857df154e4e25e8de473bf2bbfa47
Frank Schmidt <frank.schmidt@redspace.com>
2023-02-07 15:37:57 -0400

    chore: support type module

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,52 @@
- const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from'path'
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
- module.exports = {
-   mode: 'development',
-   entry: {
-     index: './src/index.js',
-   },
-   devtool: 'inline-source-map',
-   devServer: {
-     static: {
-       directory: path.join(__dirname, 'public'),
-     },
-     compress: true,
-     port: 9000,
-   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       template: 'src/index.html'
-     }),
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
-   optimization: {
-     runtimeChunk: 'single',
-   },
- };
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default env => {
+  return {
+    mode: 'development',
+    entry: {
+      index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 9000,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      }),
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    optimization: {
+      runtimeChunk: 'single',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+              ]
+            }
+          }
+        }
+      ]
+    }
+  }
+};
